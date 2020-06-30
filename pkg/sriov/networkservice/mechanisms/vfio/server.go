a5afb138d6452ac2c3e3274d66b37708fe1f1074
Sergey Semenov <sergey.semenov@xored.com>
2020-06-30 12:39:04 +0700

    Rafactor to separate kernel and vfio chain elements. Switched from storing PCI addresses in separate mechanisms to storing them into ExtraContext. Added VF number storage
    Signed-off-by: Sergey Semenov <sergey.semenov@xored.com>

---
+++ pkg/sriov/networkservice/mechanisms/vfio/server.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package vfio provides a networkservice chain element that properly handles the SR-IOV vfio Mechanism
+package vfio
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+	"github.com/pkg/errors"
+
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov"
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov/utils"
+)
+
+type vfioServer struct {
+	resourcePool *sriov.NetResourcePool
+}
+
+// NewServer - returns a new authorization networkservicemesh.NetworkServiceServers
+func NewServer(resourcePool *sriov.NetResourcePool) networkservice.NetworkServiceServer {
+	return &vfioServer{
+		resourcePool: resourcePool,
+	}
+}
+
+func (a *vfioServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	if request.GetConnection() == nil {
+		request.Connection = &networkservice.Connection{
+			Context: &networkservice.ConnectionContext{
+				ExtraContext: map[string]string{},
+			},
+		}
+	}
+	err := utils.WithVirtualFunctionsState(ctx, request.GetConnection().GetContext(), a.resourcePool)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to write virtual functions state into the connection context")
+	}
+	return next.Server(ctx).Request(ctx, request)
+}
+
+func (a *vfioServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
+	return next.Server(ctx).Close(ctx, conn)
+}
---
+++ pkg/sriov/utils/vfstate.go
@@ -0,0 +1,61 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"context"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov"
+)
+
+// WithVirtualFunctionsState adds information about free virtual functions number for each available physical function
+// on host to the ExtraContext of the ConnectionContext
+func WithVirtualFunctionsState(ctx context.Context, connCtx *networkservice.ConnectionContext, resourcePool *sriov.NetResourcePool) error {
+	resourcePool.Lock()
+	defer resourcePool.Unlock()
+
+	config := &sriov.VirtualFunctionsStateConfig{
+		Config: map[string]int{},
+	}
+
+	for _, netResource := range resourcePool.Resources {
+		pf := netResource.PhysicalFunction
+		freeVfs := getFreeVirtualFunctionsNumber(pf)
+
+		config.Config[pf.PCIAddress] = freeVfs
+	}
+
+	strCfg, err := sriov.MarshallStateConfig(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	connCtx.ExtraContext[sriov.VirtualFunctionsStateConfigKey] = strCfg
+	return nil
+}
+
+func getFreeVirtualFunctionsNumber(pf *sriov.PhysicalFunction) int {
+	freeVfs := 0
+	for _, state := range pf.VirtualFunctions {
+		if state == sriov.FreeVirtualFunction {
+			freeVfs++
+		}
+	}
+	return freeVfs
+}
