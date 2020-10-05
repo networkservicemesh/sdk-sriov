89839e7bdbf653ce8148e09f504206300b75ca68
Vladimir Popov <vladimir.popov@xored.com>
2020-10-06 01:03:05 +0700

    Rework resource.Pool
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

---
+++ pkg/sriov/resource/pool.go
@@ -0,0 +1,196 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package resource provides a resource pool for SR-IOV PCI virtual functions
+package resource
+
+import (
+	"path"
+	"sort"
+
+	"github.com/pkg/errors"
+
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov"
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov/config"
+)
+
+// TokenPool is a token.Pool interface
+type TokenPool interface {
+	Find(id string) (string, error)
+	Use(id string, names []string) error
+	StopUsing(id string) error
+}
+
+// Pool manages host SR-IOV state
+// WARNING: it is thread unsafe - if you want to use it concurrently, use some synchronization outside
+type Pool struct {
+	physicalFunctions map[string]*physicalFunction
+	virtualFunctions  map[string]*virtualFunction
+	iommuGroups       map[uint]sriov.DriverType
+	tokenPool         TokenPool
+}
+
+type physicalFunction struct {
+	tokenNames       map[string]struct{}
+	virtualFunctions map[uint][]*virtualFunction
+	freeVFsCount     int
+}
+
+type virtualFunction struct {
+	pciAddr    string
+	pfPciAddr  string
+	iommuGroup uint
+	tokenID    string
+}
+
+// NewPool returns a new Pool
+func NewPool(tokenPool TokenPool, cfg *config.Config) *Pool {
+	p := &Pool{
+		physicalFunctions: map[string]*physicalFunction{},
+		virtualFunctions:  map[string]*virtualFunction{},
+		iommuGroups:       map[uint]sriov.DriverType{},
+		tokenPool:         tokenPool,
+	}
+
+	for pfPciAddr, pff := range cfg.PhysicalFunctions {
+		pf := &physicalFunction{
+			tokenNames:       map[string]struct{}{},
+			virtualFunctions: map[uint][]*virtualFunction{},
+			freeVFsCount:     len(pff.VirtualFunctions),
+		}
+		p.physicalFunctions[pfPciAddr] = pf
+
+		for _, service := range pff.Services {
+			for _, capability := range pff.Capabilities {
+				pf.tokenNames[path.Join(service, capability)] = struct{}{}
+			}
+		}
+
+		for vfPciAddr, iommuGroup := range pff.VirtualFunctions {
+			vf := &virtualFunction{
+				pciAddr:    vfPciAddr,
+				pfPciAddr:  pfPciAddr,
+				iommuGroup: iommuGroup,
+			}
+			p.virtualFunctions[vfPciAddr] = vf
+
+			pf.virtualFunctions[iommuGroup] = append(pf.virtualFunctions[iommuGroup], vf)
+			p.iommuGroups[iommuGroup] = sriov.NoDriver
+		}
+	}
+
+	return p
+}
+
+// Select selects a virtual function for the given driver type and marks it as "in-use"
+func (p *Pool) Select(tokenID string, driverType sriov.DriverType) (string, error) {
+	tokenName, err := p.tokenPool.Find(tokenID)
+	if err != nil {
+		return "", err
+	}
+
+	vfs := p.find(driverType, tokenName)
+	if len(vfs) == 0 {
+		return "", errors.Errorf("no free VF for the driver type: %v", driverType)
+	}
+
+	sort.Slice(vfs, func(i, k int) bool {
+		iIg := p.iommuGroups[vfs[i].iommuGroup]
+		kIg := p.iommuGroups[vfs[k].iommuGroup]
+		iPf := p.physicalFunctions[vfs[i].pfPciAddr]
+		kPf := p.physicalFunctions[vfs[k].pfPciAddr]
+		switch {
+		case iIg == driverType && kIg == sriov.NoDriver:
+			return true
+		case iIg == sriov.NoDriver && kIg == driverType:
+			return false
+		default:
+			return iPf.freeVFsCount-kPf.freeVFsCount > 0
+		}
+	})
+
+	if err = p.selectVF(vfs[0], tokenID, driverType); err != nil {
+		return "", err
+	}
+
+	return vfs[0].pciAddr, nil
+}
+
+func (p *Pool) find(driverType sriov.DriverType, tokenName string) []*virtualFunction {
+	var virtualFunctions []*virtualFunction
+	for _, pf := range p.physicalFunctions {
+		if _, ok := pf.tokenNames[tokenName]; ok {
+			for iommuGroup, vfs := range pf.virtualFunctions {
+				if ig := p.iommuGroups[iommuGroup]; ig == sriov.NoDriver || ig == driverType {
+					for _, vf := range vfs {
+						if vf.tokenID == "" {
+							virtualFunctions = append(virtualFunctions, vf)
+						}
+					}
+				}
+			}
+		}
+	}
+	return virtualFunctions
+}
+
+func (p *Pool) selectVF(vf *virtualFunction, tokenID string, driverType sriov.DriverType) error {
+	var tokenNames []string
+	for tokenName := range p.physicalFunctions[vf.pfPciAddr].tokenNames {
+		tokenNames = append(tokenNames, tokenName)
+	}
+	if err := p.tokenPool.Use(tokenID, tokenNames); err != nil {
+		return err
+	}
+
+	vf.tokenID = tokenID
+
+	p.physicalFunctions[vf.pfPciAddr].freeVFsCount--
+	p.iommuGroups[vf.iommuGroup] = driverType
+
+	return nil
+}
+
+// Free marks given virtual function as "free" and binds it to the "NoDriver" driver type
+func (p *Pool) Free(vfPciAddr string) error {
+	vf, ok := p.virtualFunctions[vfPciAddr]
+	if !ok {
+		return errors.Errorf("VF doesn't exist: %v", vfPciAddr)
+	}
+
+	if vf.tokenID == "" {
+		return errors.Errorf("trying to free not selected VF: %v", vf.pciAddr)
+	}
+	if err := p.tokenPool.StopUsing(vf.tokenID); err != nil {
+		return err
+	}
+	vf.tokenID = ""
+
+	p.physicalFunctions[vf.pfPciAddr].freeVFsCount++
+
+	for _, pf := range p.physicalFunctions {
+		if vffs, ok := pf.virtualFunctions[vf.iommuGroup]; ok {
+			for _, vff := range vffs {
+				if vff.tokenID != "" {
+					return nil
+				}
+			}
+		}
+	}
+	p.iommuGroups[vf.iommuGroup] = sriov.NoDriver
+
+	return nil
+}
---
+++ pkg/sriov/resource/pool_test.go
@@ -0,0 +1,165 @@
+// Copyright (c) 2020 Doc.ai and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resource_test
+
+import (
+	"context"
+	"path"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov"
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov/config"
+	"github.com/networkservicemesh/sdk-sriov/pkg/sriov/resource"
+)
+
+const (
+	configFileName  = "config.yml"
+	service1        = "service-1"
+	service2        = "service-2"
+	capabilityIntel = "intel"
+	capability10G   = "10G"
+	vf11PciAddr     = "0000:01:00.1"
+	vf21PciAddr     = "0000:02:00.1"
+	vf22PciAddr     = "0000:02:00.2"
+	vf31PciAddr     = "0000:03:00.1"
+)
+
+func TestPool_Select_Service(t *testing.T) {
+	tokenPool := &tokenPoolStub{
+		tokens: map[string]string{
+			"1": path.Join(service1, capabilityIntel),
+		},
+	}
+
+	cfg, err := config.ReadConfig(context.TODO(), configFileName)
+	require.NoError(t, err)
+
+	p := resource.NewPool(tokenPool, cfg)
+
+	vfPciAddr, err := p.Select("1", sriov.VfioPCIDriver)
+	assert.Nil(t, err)
+	assert.Equal(t, vf11PciAddr, vfPciAddr)
+}
+
+func TestPool_Select_Capability(t *testing.T) {
+	tokenPool := &tokenPoolStub{
+		tokens: map[string]string{
+			"1": path.Join(service2, capability10G),
+		},
+	}
+
+	cfg, err := config.ReadConfig(context.TODO(), configFileName)
+	require.NoError(t, err)
+
+	p := resource.NewPool(tokenPool, cfg)
+
+	vfPciAddr, err := p.Select("1", sriov.VfioPCIDriver)
+	assert.Nil(t, err)
+	assert.Equal(t, vf21PciAddr, vfPciAddr)
+}
+
+func TestPool_Select_DriverType(t *testing.T) {
+	tokenPool := &tokenPoolStub{
+		tokens: map[string]string{
+			"1": path.Join(service1, capabilityIntel),
+			"2": path.Join(service2, capabilityIntel),
+		},
+	}
+
+	cfg, err := config.ReadConfig(context.TODO(), configFileName)
+	require.NoError(t, err)
+
+	p := resource.NewPool(tokenPool, cfg)
+
+	vfPciAddr, err := p.Select("1", sriov.VfioPCIDriver)
+	assert.Nil(t, err)
+	assert.Equal(t, vf11PciAddr, vfPciAddr)
+
+	vfPciAddr, err = p.Select("2", sriov.KernelDriver)
+	assert.Nil(t, err)
+	assert.Equal(t, vf22PciAddr, vfPciAddr)
+}
+
+func TestPool_Select_FreeVFsCount(t *testing.T) {
+	tokenPool := &tokenPoolStub{
+		tokens: map[string]string{
+			"1": path.Join(service2, capabilityIntel),
+		},
+	}
+
+	cfg, err := config.ReadConfig(context.TODO(), configFileName)
+	require.NoError(t, err)
+
+	p := resource.NewPool(tokenPool, cfg)
+
+	vfPciAddr, err := p.Select("1", sriov.VfioPCIDriver)
+	assert.Nil(t, err)
+	assert.Equal(t, vf31PciAddr, vfPciAddr)
+}
+
+func TestPool_Free(t *testing.T) {
+	tokenPool := &tokenPoolStub{
+		tokens: map[string]string{
+			"1": path.Join(service1, capabilityIntel),
+		},
+	}
+
+	cfg, err := config.ReadConfig(context.TODO(), configFileName)
+	require.NoError(t, err)
+
+	p := resource.NewPool(tokenPool, cfg)
+
+	vfPciAddr, err := p.Select("1", sriov.VfioPCIDriver)
+	assert.Nil(t, err)
+	assert.Equal(t, vf11PciAddr, vfPciAddr)
+
+	err = p.Free(vfPciAddr)
+	assert.Nil(t, err)
+
+	vfPciAddr, err = p.Select("1", sriov.VfioPCIDriver)
+	assert.Nil(t, err)
+	assert.Equal(t, vf11PciAddr, vfPciAddr)
+}
+
+type tokenPoolStub struct {
+	tokens map[string]string
+}
+
+func (tp *tokenPoolStub) Find(id string) (string, error) {
+	if tokenName, ok := tp.tokens[id]; ok {
+		return tokenName, nil
+	}
+	return "", errors.New("invalid token ID")
+}
+
+func (tp *tokenPoolStub) Use(id string, _ []string) error {
+	if _, ok := tp.tokens[id]; ok {
+		return nil
+	}
+	return errors.New("invalid token ID")
+}
+
+func (tp *tokenPoolStub) StopUsing(id string) error {
+	if _, ok := tp.tokens[id]; ok {
+		return nil
+	}
+	return errors.New("invalid token ID")
+}
