840d381b0c9c293e340947194d9f593eb0efc0ba
Vladimir Popov <vladimir.popov@xored.com>
2020-10-20 16:48:30 +0700

    Update CI files, set go 1.15
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/networkservicemesh/sdk-sriov
 
-go 1.13
+go 1.15
 
 require (
 	github.com/ghodss/yaml v1.0.0
--- pkg/sriov/sriovtest/file_api.go
+++ pkg/sriov/sriovtest/file_api.go
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//+build !windows
+
 // Package sriovtest provides utils for SR-IOV testing
 package sriovtest
 
