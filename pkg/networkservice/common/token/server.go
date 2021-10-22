1f7252022137f2170b6dc4b179bb06b57f0c8821
Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>
2021-10-22 13:54:27 +0200

    split token package into multitoken and sharedtoken
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>

--- pkg/networkservice/common/token/server.go
+++ pkg/networkservice/common/token/server.go
@@ -21,72 +21,20 @@
 package token
 
 import (
-	"context"
 	"os"
 
-	"github.com/golang/protobuf/ptypes/empty"
-
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
-	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/kernel"
-	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
 
+	"github.com/networkservicemesh/sdk-sriov/pkg/networkservice/common/token/multitoken"
+	"github.com/networkservicemesh/sdk-sriov/pkg/networkservice/common/token/sharedtoken"
 	"github.com/networkservicemesh/sdk-sriov/pkg/tools/tokens"
 )
-
-type tokenServer struct {
-	tokenName   string
-	sharedToken string
-	config      tokenConfig
-}
 
 // NewServer returns a new token server chain element for the given tokenKey
 func NewServer(tokenKey string) networkservice.NetworkServiceServer {
 	sriovTokens := tokens.FromEnv(os.Environ())[tokenKey]
 	if len(sriovTokens) == 1 {
-		return &tokenServer{
-			sharedToken: sriovTokens[0],
-		}
+		return sharedtoken.NewServer(sriovTokens[0])
 	}
-	return &tokenServer{
-		tokenName: tokenKey,
-		config: createTokenElement(map[string][]string{
-			tokenKey: sriovTokens,
-		}),
-	}
-}
-
-func (s *tokenServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
-	var isEstablished bool
-	var tokenID string
-	if s.isMultiToken() {
-		isEstablished = s.config.get(request.GetConnection()) != ""
-	}
-	mechanism := kernel.ToMechanism(request.GetConnection().GetMechanism())
-	if mechanism != nil && mechanism.GetDeviceTokenID() == "" {
-		if !s.isMultiToken() {
-			mechanism.SetDeviceTokenID(s.sharedToken)
-		} else if tokenID = s.config.assign(s.tokenName, request.GetConnection()); tokenID != "" {
-			mechanism.SetDeviceTokenID(tokenID)
-		}
-	} else if mechanism != nil && mechanism.GetDeviceTokenID() != "" {
-		isEstablished = true
-	}
-
-	conn, err := next.Server(ctx).Request(ctx, request)
-	if err != nil && tokenID != "" && !isEstablished {
-		s.config.release(request.GetConnection())
-	}
-
-	return conn, err
-}
-
-func (s *tokenServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
-	if mechanism := kernel.ToMechanism(conn.GetMechanism()); mechanism != nil {
-		s.config.release(conn)
-	}
-	return next.Server(ctx).Close(ctx, conn)
-}
-
-func (s *tokenServer) isMultiToken() bool {
-	return s.config != nil
+	return multitoken.NewServer(tokenKey)
 }
---
+++ pkg/networkservice/common/token/sharedtoken/server.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2021 Nordix Foundation.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build linux
+
+// Package sharedtoken provides server chain element for inserting shared SRIOV token into request and response
+package sharedtoken
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
+	"github.com/networkservicemesh/api/pkg/api/networkservice"
+	"github.com/networkservicemesh/api/pkg/api/networkservice/mechanisms/kernel"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
+)
+
+type tokenServer struct {
+	sharedToken string
+}
+
+// NewServer returns a new shard token server chain element for the given token
+func NewServer(token string) networkservice.NetworkServiceServer {
+	return &tokenServer{
+		sharedToken: token,
+	}
+}
+
+func (s *tokenServer) Request(ctx context.Context, request *networkservice.NetworkServiceRequest) (*networkservice.Connection, error) {
+	mechanism := kernel.ToMechanism(request.GetConnection().GetMechanism())
+	if mechanism != nil && mechanism.GetDeviceTokenID() == "" {
+		mechanism.SetDeviceTokenID(s.sharedToken)
+	}
+	return next.Server(ctx).Request(ctx, request)
+}
+
+func (s *tokenServer) Close(ctx context.Context, conn *networkservice.Connection) (*empty.Empty, error) {
+	return next.Server(ctx).Close(ctx, conn)
+}
